c26d67b1bba188a52945c0eb41c55649c0a8adb9
Implycitt <qgbordelon@gmail.com>
2024-02-04 13:50:57 -0600

    good enough

--- main.js
+++ main.js
@@ -10,4 +10,18 @@ function hearts() {
   container.appendChild(create);
 }
 
-setInterval(hearts, 600);
+document.getElementById("yes").onclick = function () { 
+  location.href = "yay.html";
+};
+
+const maxWidth = 600;
+const maxHeight = 600;
+
+const button = document.getElementById('no');
+button.addEventListener('mouseover', function() {
+  button.style.left = Math.floor(Math.random() * (maxWidth + 1)) + 'px';
+  button.style.top = Math.floor(Math.random() * (maxWidth + 1)) + 'px';
+})
+
+setInterval(hearts, 1000);
+
